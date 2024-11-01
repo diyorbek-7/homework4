d3206a6cff394a844380df2b75aeb1027a7689ba
diyorbek-7 <d.shokirov@newuu.uz>
2024-11-01 16:33:48 +0500

    Add files via upload

---
+++ problems.h
@@ -0,0 +1,26 @@
+
+//In the comments after each problem functions' names are provided. Your task to define correct datatype and parameters.
+//DO NOT CHANGE FUNCTIONS' NAMES.
+//You may check your functions in main.cpp as shown by exampleFunction;
+void exampleFunction() {
+    std::cout<<"Example Function that can be called in main."<<std::endl;
+}
+
+// Problem 1
+//floorFunction
+//ceilingFunction
+
+// Problem 2
+//swap_values
+
+// Problem 3
+//multiply
+
+// Problem 4
+//digitSum
+
+// Problem 5
+//decimalToBinary
+
+// Problem 6
+//midValue
